2c840ae98fc9ea90fcd59d2a7f5f9547f283d9b4
Gordon <ko2761oe@ko2761oem8.workstations.winona.edu>
2018-10-25 11:22:55 -0500

    imported vis.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Services
+import { VisService } from './vis/vis.service';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [VisService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/vis/vis.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import * as vis from 'vis';
+@Injectable({
+  providedIn: 'root'
+})
+export class VisService {
+  constructor() {}
+}
