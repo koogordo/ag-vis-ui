1d4d51f7c99f952dbce7d5b4903ad23c5419afa4
xxauroraxx <aurorapariseau@gmail.com>
2018-11-01 10:55:09 -0500

    Add files via upload
    changed '_activated' property to '_active'

--- devicescripts/relay.py
+++ devicescripts/relay.py
@@ -10,7 +10,7 @@ class Relay:
     #self.__var  Private variable
     def __init__(self, pin):
         
-        self._activated = False
+        self._active = False
         self._pin = pin
         self._type = 'relay'
         
@@ -19,8 +19,8 @@ class Relay:
         self.off()
         
     @property
-    def activated(self):
-        return self._activated
+    def active(self):
+        return self._active
         
     @property
     def pin(self):
@@ -34,10 +34,10 @@ class Relay:
 
         try:
             GPIO.output(self._pin, 1)
-            self._activated = False
+            self._active = False
             
         except Exception as e:
-            self._activated = None
+            self._active = None
             print "There was an error turning off the relay\n\t" + str(e)
             
     def on(self, duration=None):
@@ -45,7 +45,7 @@ class Relay:
         try:
             if not duration:
                 GPIO.output(self._pin, 0)
-                self._activated = True
+                self._active = True
             else:
                 if duration > LOWER_BOUND and duration < UPPER_BOUND:
                     self.on()
@@ -55,5 +55,5 @@ class Relay:
                     print("Time is out of range [%s,%s]. Found %s"%(LOWER_BOUND,UPPER_BOUND,duration))
             
         except Exception as e:
-            self._activated = None
+            self._active = None
             print "There was an error turning on the relay\n\t" + str(e)
