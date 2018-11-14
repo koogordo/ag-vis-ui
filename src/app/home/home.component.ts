e45df5ac6ff496fd245e481439d55d6cd6b3610d
Kieran Gordon <koo.m.gordon@outlook.com>
2018-11-13 21:02:17 -0600

    error message outputs and input error highlighting add

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,33 @@ declare var $: any;
 })
 export class HomeComponent implements OnInit {
   private loginForm = new FormGroup({
-    email: new FormControl('', Validators.required),
+    email: new FormControl(
+      '',
+      Validators.compose([Validators.required, Validators.email])
+    ),
     password: new FormControl('', Validators.required)
   });
   private registerForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
+    firstName: new FormControl(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(100)
+      ])
+    ),
+    lastName: new FormControl(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.min(3),
+        Validators.max(100)
+      ])
+    ),
+    email: new FormControl(
+      '',
+      Validators.compose([Validators.required, Validators.email])
+    ),
     password: new FormControl('', Validators.required),
     confPass: new FormControl('', Validators.required)
   });
@@ -23,22 +43,42 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {}
 
-  login() {
+  public login() {
     /*
      * TODO
      * validate form
      * call backend to log user in
      * get login result and act accordingly
      */
-    console.log(this.loginForm);
+    console.log(this.loginForm.controls.email.errors);
+    console.log(this.loginForm.controls.password.errors);
   }
-  register() {
+  public register() {
     /*
      * TODO
      * validate form
      * call backend to register user
      * get register result and act accordingly
      */
-    console.log(this.registerForm);
+    console.log(this.registerForm.controls.firstName.errors);
+  }
+
+  private setErrorMessage(errors) {
+    if (errors.required) {
+      return 'This field cannot be blank.';
+    }
+    if (errors.email) {
+      return 'Must have a valid email address.';
+    }
+    if (errors.minlength) {
+      return `Name must be at least ${
+        errors.minlength.requiredLength
+      } characters.`;
+    }
+    if (errors.maxlength) {
+      return `Name can only have a maximum of ${
+        errors.maxlength.requiredLength
+      } characters.`;
+    }
   }
 }
