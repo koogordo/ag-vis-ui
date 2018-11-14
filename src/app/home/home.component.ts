1995407da9995f2d13e52473e4f1d24bb5e6daf1
Kieran Gordon <koo.m.gordon@outlook.com>
2018-11-13 19:41:02 -0600

    added TODO anchors for login and register methods of home coomponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,24 +9,36 @@ declare var $: any;
 })
 export class HomeComponent implements OnInit {
   private loginForm = new FormGroup({
-    email: new FormControl(),
-    password: new FormControl()
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
   });
   private registerForm = new FormGroup({
-    firstName: new FormControl(),
-    lastName: new FormControl(),
-    email: new FormControl(),
-    password: new FormControl(),
-    confPass: new FormControl()
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    confPass: new FormControl('', Validators.required)
   });
   constructor() {}
 
   ngOnInit() {}
 
   login() {
+    /*
+     * TODO
+     * validate form
+     * call backend to log user in
+     * get login result and act accordingly
+     */
     console.log(this.loginForm);
   }
   register() {
+    /*
+     * TODO
+     * validate form
+     * call backend to register user
+     * get register result and act accordingly
+     */
     console.log(this.registerForm);
   }
 }
