2e1990a87a4cf38f55ea76a5e88ab8e7544d50e3
gunnerganske <gganske15@winona.edu>
2018-12-02 12:51:22 -0600

    Bar and Line Charts Working

--- src/app/my-bar-chart/my-bar-chart.component.ts
+++ src/app/my-bar-chart/my-bar-chart.component.ts
@@ -9,19 +9,86 @@ export class MyBarChartComponent implements OnInit {
   @Input() chartData;
   public barChartOptions = {
     scaleShowVerticalLines: false,
-    responsive: true
+    responsive: true,
+    autoskip: true
   };
 
   public barChartLabels = [];
   public barChartType = 'bar';
   public barChartLegend = true;
 
-  public barChartData = [
-    { data: [65, 59, 80, 97], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-  ];
+  public barChartData = [];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.chartData[0].type === "DHT11") {
+      this.dht11Chart();
+    } else if (this.chartData[0].type === "watersensor") {
+      this.watersensorChart();
+    } else if (this.chartData[0].type === "relay") {
+      this.relayChart();
+    }
+  }
+
+  public dht11Chart() {
+    const temp = [];
+    const humidity = [];
+    const labels = [];
+    for (let i = 0; i < this.chartData.length; i++) {
+         if (i % 500 === 0) {
+          const value = this.chartData[i].value;
+          const date = new Date(this.chartData[i].time * 1000);
+          labels.push(date.getDate());
+          const res = value.split(",");
+          const trimmedRes = res.map((item) => {
+          const half = item.replace('(', '');
+          const full = half.replace(')', '');
+          return full;
+          });
+       temp.push(trimmedRes[0]);
+       humidity.push(trimmedRes[1]);
+        }
+      }
+      this.barChartLabels = labels;
+      console.log(temp);
+     this.barChartData = [ { data: temp, label: 'Temperature' }, { data: humidity, label: 'Humidity' } ];
+    console.log(this.chartData[0]);
+  }
+
+  public watersensorChart() {
+    const labels = [];
+    const level = [];
+
+    for (let i = 0; i < this.chartData.length; i++) {
+      if (i % 500 === 0) {
+        const value = this.chartData[i].value;
+        const date = new Date(this.chartData[i].time * 1000);
+        labels.push(date.getDate());
+        level.push(value);
+      }
+    }
+    this.barChartLabels = labels;
+    console.log(level);
+    this.barChartData =  [ { data: level, label: 'Water Level' } ];
+    console.log(this.chartData[0]);
+  }
+
+  public relayChart() {
+    const labels = [];
+    const relay = [];
+
+    for (let i = 0; i < this.chartData.length; i++) {
+      if (i % 500 === 0) {
+        const value = this.chartData[i].value;
+        const date = new Date(this.chartData[i].time * 1000);
+        labels.push(date.getDate());
+        relay.push(value);
+      }
+    }
+    console.log(relay);
+    this.barChartLabels = labels;
+    this.barChartData = [ { data: relay, label: 'Relay' } ];
+    console.log(this.chartData[0]);
+  }
 }
--- src/app/my-doughnut-chart/my-doughnut-chart.component.ts
+++ src/app/my-doughnut-chart/my-doughnut-chart.component.ts
@@ -7,11 +7,80 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MyDoughnutChartComponent implements OnInit {
   @Input() chartData;
-  public doughnutChartLabels = ['Sales 1', 'Sales 2', 'Sales 3', 'Sales 4'];
+  public doughnutChartLabels = [];
 
-  public doughnutChartData = [120, 150, 180, 90];
+  public doughnutChartData = [];
   public doughnutChartType = 'doughnut';
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.chartData[0].type === "DHT11") {
+      this.dht11Chart();
+    } else if (this.chartData[0].type === "watersensor") {
+      this.watersensorChart();
+    } else if (this.chartData[0].type === "relay") {
+      this.relayChart();
+    }
+  }
+
+  public dht11Chart() {
+    const temp = [];
+    const humidity = [];
+    const labels = [];
+    for (let i = 0; i < this.chartData.length; i++) {
+         if (i % 500 === 0) {
+          const value = this.chartData[i].value;
+          const date = new Date(this.chartData[i].time * 1000);
+          labels.push(date.getDate());
+          const res = value.split(",");
+          const trimmedRes = res.map((item) => {
+          const half = item.replace('(', '');
+          const full = half.replace(')', '');
+          return full;
+          });
+       temp.push(trimmedRes[0]);
+       humidity.push(trimmedRes[1]);
+        }
+      }
+      this.doughnutChartLabels = labels;
+      console.log(temp);
+     this.doughnutChartData = [ { data: temp, label: 'Temperature' }, { data: humidity, label: 'Humidity' } ];
+    console.log(this.chartData[0]);
+  }
+
+  public watersensorChart() {
+    const labels = [];
+    const level = [];
+
+    for (let i = 0; i < this.chartData.length; i++) {
+      if (i % 500 === 0) {
+        const value = this.chartData[i].value;
+        const date = new Date(this.chartData[i].time * 1000);
+        labels.push(date.getDate());
+        level.push(value);
+      }
+    }
+    this.doughnutChartLabels = labels;
+    console.log(level);
+    this.doughnutChartData =  [ { data: level, label: 'Water Level' } ];
+    console.log(this.chartData[0]);
+  }
+
+  public relayChart() {
+    const labels = [];
+    const relay = [];
+
+    for (let i = 0; i < this.chartData.length; i++) {
+      if (i % 500 === 0) {
+        const value = this.chartData[i].value;
+        const date = new Date(this.chartData[i].time * 1000);
+        labels.push(date.getDate());
+        relay.push(value);
+      }
+    }
+    console.log(relay);
+    this.doughnutChartLabels = labels;
+    this.doughnutChartData = [ { data: relay, label: 'Relay' } ];
+    console.log(this.chartData[0]);
+  }
 }
