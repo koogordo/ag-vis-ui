4a956fd04fcffac92eca530c9d36fc9ee0697bfc
gunnerganske <gganske15@winona.edu>
2018-12-01 18:26:02 -0600

    chart data

--- src/app/my-line-chart/my-line-chart.component.ts
+++ src/app/my-line-chart/my-line-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+
 @Component({
   selector: 'app-my-line-chart',
   templateUrl: './my-line-chart.component.html',
@@ -9,25 +10,40 @@ export class MyLineChartComponent implements OnInit {
   @Input() chartData;
   public lineChartOptions = {
     scaleShowVerticalLines: false,
-    responsive: true
+    responsive: true,
   };
 
   public lineChartLabels = [];
   public lineChartType = 'line';
   public lineChartLegend = true;
 
-  public lineChartData = [
-    { data: [65, 59, 80, 97], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-  ];
+  public lineChartData = [];
 
   constructor() {}
 
   ngOnInit() {
-    this.lineChartLabels = this.chartData.map(reading => {
-      const date = new Date(reading.time);
-      return date.getDate();
-    });
+    const temp = [];
+    const humidity = [];
+    const labels = [];
+    for (let i = 0; i < this.chartData.length; i++) {
+         if (i % 50 === 0) {
+          const value = this.chartData[i].value;
+          const date = new Date(this.chartData[i].time);
+          labels.push(date.getMinutes());
+          const res = value.split(",");
+          const trimmedRes = res.map((item) => {
+          const half = item.replace('(', '');
+          const full = half.replace(')', '');
+          return full;
+          });
+       temp.push(trimmedRes[0]);
+       humidity.push(trimmedRes[1]);
+        }
+      }
+      console.log(temp);
+      this.lineChartLabels = labels;
+     this.lineChartData = [ { data: temp, label: 'Temperature' }, { data: humidity, label: 'Humidity' } ];
     console.log(this.chartData[0]);
   }
+
 }
